787172c256817c4aa455c2c0c06225c31032913b
= <=>
2016-05-07 00:04:36 +0200

    add Redis use

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,40 +1,7 @@
 <?php
-// class Mailer
-// {
 
-// }
-// class Regmailerers
-// {
-// 	private $mailer;
+use Illuminate\Support\Facades\Redis;
 
-// 	public function __construct(Mailer $mailer)
-// 	{
-// 		$this->mailer = $mailer;
-// 	}
-
-// 	public function setMailer(Mailer $mailer) {
-// 		$this->mailer = $mailer;
-// 	} 
-// }
-
-// App::singleton('foo', function() {
-// 	return new RegistersUsers(new Mailer());
-// });
-
-// $one = app('foo');
-// $two = app('foo');
-
-// var_dump($one, $two);
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 Route::get('/', function () {
 	return view('welcome');
 });
